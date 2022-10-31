14117fe51a475cd3733b9b7a9a84e37284bcbbcd
jesue <jesue_gege@hotmail.com>
2022-10-31 20:10:33 -0300

    5 finalizada

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let teste = {};
+  if (!animal) {
+    data.species.forEach((specie) => {
+      const { name, residents } = specie;
+      teste[name] = residents.length;
+    });
+    return teste;
+  }
+  const animals = data.species.find((specie) => specie
+    .name === animal.specie);
+  if (!animal.sex) {
+    teste = animals.residents.length;
+  } else {
+    teste = animals.residents.filter((resident) => resident
+      .sex === animal.sex).length;
+  }
+
+  return teste;
 }
+
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 
 module.exports = countAnimals;
