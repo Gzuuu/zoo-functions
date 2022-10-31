fe923f7e3a1505a7a654c3a8bffc3ac9f63476d3
jesue <jesue_gege@hotmail.com>
2022-10-31 15:07:55 -0300

    1 finalizada

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const result = [];
+  ids.forEach((element) => {
+    data.species.forEach((specie) => {
+      if (element === specie.id) {
+        result.push(specie);
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = getSpeciesByIds;
