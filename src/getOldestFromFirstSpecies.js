8b5e9615afd2929ca6c1f89d222f82cafffa1dc1
jesue <jesue_gege@hotmail.com>
2022-11-01 16:43:24 -0300

    10 concluida

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pessoa = data.employees.find((employee) => employee.id === id);
+  const animal = data.species.find((specie) => specie.id === pessoa.responsibleFor[0]);
+  const maisVelho = animal.residents.reduce((acc, cur) => ((cur.age > acc.age) ? cur : acc));
+  const { age, sex, name } = maisVelho;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
