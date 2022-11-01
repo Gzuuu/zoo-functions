a24de9b715f9a893a647616f7bf865edc4f6877a
jesue <jesue_gege@hotmail.com>
2022-11-01 12:21:35 -0300

    8 concluida

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(entrantsCount) {
+  const result = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrantsCount.forEach((entrant) => {
+    if (entrant.age < 18) {
+      result.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      result.adult += 1;
+    } else result.senior += 1;
+  });
+
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  const visitors = countEntrants(entrants);
+  const sum = visitors.adult * 49.99
+    + visitors.child * 20.99
+    + visitors.senior * 24.99;
+  return sum;
 }
-
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
